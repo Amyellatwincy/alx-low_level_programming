684136a11b0eba0229223f9e5f823d8bfa361475
Amyellatwincy <emmanuellaokonkwo.oe@gmail.com>
2022-06-30 19:52:55 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase " programming is like a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
