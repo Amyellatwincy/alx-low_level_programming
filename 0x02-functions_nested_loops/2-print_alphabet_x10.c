6db596f0cbd20b99fb6dd811f3e0a3cc5d03e596
Amyellatwincy <emmanuellaokonkwo.oe@gmail.com>
2022-07-05 15:27:12 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> prints the lowercase alphabets
+*/
+
+void print_alphabet_x10(void)
+{
+int m;
+int p;
+
+for (p = 0; p < 10; p++)
+{
+for (m = 'a'; m <= 'z'; m++)
+{
+_putchar(m);
+}
+putchar('\n');
+}
+}
