4f90de253504a422e764587bb816e8147e838bd1
Amyellatwincy <emmanuellaokonkwo.oe@gmail.com>
2022-07-08 12:44:44 -0700

    task 10 final

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle, followed by a new line
- * @size: size of the triangle
+ * print_triangle - check for a digit
+ * @size: integer type
  * Return: void
  */
 
