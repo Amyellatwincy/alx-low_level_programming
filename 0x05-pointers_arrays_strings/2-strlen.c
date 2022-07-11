1c00624b78150aab0dff5b4fe7c424f1906b180e
Amyellatwincy <emmanuellaokonkwo.oe@gmail.com>
2022-07-11 15:50:11 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _stelen - function to get the length of a string
+ * @s: string pointre to passed to this function
+ * Return: returns length of the string
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
