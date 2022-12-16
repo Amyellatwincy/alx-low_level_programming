a30ac6028a769daf0acbed1ac008776eec8f1d84
Amyellatwincy <emmanuellaokonkwo.oe@gmail.com>
2022-12-16 13:47:51 +0000

    task finish

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - this is a function to check upper case alphas
+ * @c: an interger parameter
+ * Return: give 1 or 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c  <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - copiar el contendio de uno a otra variable
+ * @dest: This is destiny
+ * @src: This is the copia
+ * Return: thi return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *start = dest;
+
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (start);
+}
